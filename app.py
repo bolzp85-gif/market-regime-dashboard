9b722f86091593377a73dd7876ac1a9cd21f9c32
bolzp85-gif <bolzp85@aol.de>
2026-08-19 14:07:42 +0200

    app.py erstellen

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Streamlit Test",
+    page_icon="✅"
+)
+
+st.title("✅ STREAMLIT TEST")
+st.success("Streamlit läuft!")
+st.write("Wenn du das siehst, funktioniert die Streamlit-App grundsätzlich.")
